73511f795a684737e7598068ef6d39f8cb994032
Shahrad Elahi <shahradq@gmail.com>
2024-06-26 09:30:16 +0330

    fix #10

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,8 @@ export default defineConfig([
   {
     clean: true,
     sourcemap: true,
+    dts: true,
+    keepNames: true,
     entry: ['src/*.ts', 'src/*.tsx', '!src/*.test.ts'],
     format: ['cjs', 'esm'],
     target: 'esnext',
