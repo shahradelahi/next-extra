edad6fdeae588890856ad3ff22bae498585de688
Shahrad Elahi <shahrad@litehex.com>
2024-10-22 08:35:47 -0900

    fix: remove `.map` and `src/*` from dist (#49)

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup';
 export default defineConfig([
   {
     clean: true,
-    sourcemap: true,
     dts: true,
     entry: ['src/*.ts', 'src/*.tsx', '!src/*.test.ts', '!src/*.test.tsx'],
     format: ['cjs', 'esm'],
