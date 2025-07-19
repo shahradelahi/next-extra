eba46558167b5e5513d13d535223d6af2011ad99
Shahrad Elahi <shahrad@litehex.com>
2025-07-19 12:06:51 +0100

    chore: Update tsconfig (#70)

--- src/utils/dynamic-import.ts
+++ src/utils/dynamic-import.ts
@@ -2,7 +2,6 @@ export function safeImport<T = object>(path: string): T | undefined {
   try {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     return require(path);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (_) {
     return;
   }
