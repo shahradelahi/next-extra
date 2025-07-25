360f8548efea8c4518f4aac70bfa4414e481c57f
Shahrad Elahi <shahrad@litehex.com>
2025-07-25 01:55:15 +0100

    fix(async-storage): Add support for `workUnitAsyncStorage`
    The internal API for accessing the request store in Next.js has changed in recent canary versions. This commit updates the logic to support both the `getExpectedRequestStore` function and the new `workUnitAsyncStorage` property.
    
    Closes #75

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig([
-  {
-    clean: true,
-    dts: true,
-    entry: ['src/*.ts', 'src/*.tsx', '!src/*.test.ts', '!src/*.test.tsx'],
-    format: ['cjs', 'esm'],
-    target: 'esnext',
-    outDir: 'dist',
-  },
-]);
+export default defineConfig((options) => ({
+  clean: !options.watch,
+  dts: true,
+  entry: ['src/*.ts', 'src/*.tsx', '!src/*.test.ts', '!src/*.test.tsx'],
+  format: ['cjs', 'esm'],
+  target: 'esnext',
+  outDir: 'dist',
+}));
