eeb1b0a04669dbab4a1a267539cded69140260d1
Shahrad Elahi <shahrad@litehex.com>
2026-06-01 14:47:27 +0100

    fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => ({
   clean: !options.watch,
-  dts: true,
+  dts: {
+    compilerOptions: {
+      ignoreDeprecations: '6.0',
+    },
+  },
   splitting: true,
   entry: ['src/*.ts', 'src/*.tsx', '!src/*.test.ts', '!src/*.test.tsx'],
   format: ['cjs', 'esm'],
